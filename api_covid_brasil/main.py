aa247864f6ab1a30b92d8e35855eae77caeb660d
Rodrigo Bellizzieri <rodrigo.bellizzieri@hotmail.com>
2021-05-29 21:08:43 -0300

    Inicial

---
+++ api_covid_brasil/main.py
@@ -0,0 +1,16 @@
+import requests
+from requests import status_codes
+from requests.exceptions import FileModeWarning
+from requests.models import HTTPBasicAuth
+
+url = 'https://imunizacao-es.saude.gov.br/_search'
+user = 'imunizacao_public'
+password = 'qlto5t&7r_@+#Tlstigi'
+
+r = requests.get(url, auth=HTTPBasicAuth(user, password))
+
+resultado = r.json()
+
+
+for i in resultado['hits']['hits']:
+    print(i['_source']['vacina_lote'])
